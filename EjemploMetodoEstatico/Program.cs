81e6e555c7da2807367405fb1ad6237a5955cf5b
ciminoce <ciminoce@yahoo.com.ar>
2022-06-07 10:24:04 -0300

    Agregar archivos de proyecto.

---
+++ EjemploMetodoEstatico/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploMetodoEstatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Temperatura(10);
+            var t2=new Temperatura(15);
+            
+            Console.WriteLine(Temperatura.GetCantidad());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EjemploMetodoEstatico/Temperatura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploMetodoEstatico
+{
+    public class Temperatura
+    {
+        private static int cantidad;
+        public double Valor { get; set; }
+        public Temperatura(double valor)
+        {
+            cantidad++;
+            Valor = valor;
+        }
+
+        public double GetValor() => Valor;
+        public static int GetCantidad()
+        {
+            return cantidad;
+        }
+    }
+}
